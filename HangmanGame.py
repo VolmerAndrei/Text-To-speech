dfa20a2aa672163560b6422e05f5f2bcd3bb1150
VolmerAndrei <volmerandrei@gmail.com>
2024-03-26 09:46:30 +0200

    Signed-off-by: VolmerAndrei <volmerandrei@gmail.com> Commit

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,6 +1,68 @@
 import random
 import requests
 import csv
+
+def FunctionHiddenWord():
+    try:
+        response=requests.get('https://random-word-api.herokuapp.com/word')
+        words=response.json()
+        word=random.choice(words)
+    except requests.exceptions.ConnectionError as err:
+        print("Connection Error")
+        path=r'F:\Teme Programare\HangmanGame\Words.csv'
+        with open(path, 'r') as f:
+            reader=csv.reader(f)
+            words=random.choice(list(reader))
+            word = random.choice(words)
+    return word
+
+def FunctionEmptyWord(word):
+    hiddenWord=[]
+    x=0
+    while x<len(word):
+        hiddenWord.append("_ ")
+        x=x+1
+    return hiddenWord
+
+def FunctionUserInput(letter, usedLetters, tryes):
+    while letter==0:
+        userInput=str(input())
+        if len(userInput)==1:
+            if userInput in word and userInput not in usedLetters:
+                letter=1
+                usedLetters.append(userInput)
+                for y in range(len(word)):
+                    if word[y]==userInput:
+                        hiddenWord[y]=userInput    
+            elif userInput in usedLetters:
+                print("You already tried this one!")
+            else:
+                tryes=tryes+1
+                usedLetters.append(userInput)
+            break
+        else:
+            print("Please enter a single character!")  
+    return usedLetters, tryes
+
+def FunctionWinOrLose(win):
+    if win==1:
+        print("*** Good Job!!!  You win!!! ***")
+    else:
+        print("The word was **", word, "**")
+        print("You lose!!! :(")
+        
+def FunctionPrintHangMan(tryes):
+    print(HANGMAN_STEPS[tryes])
+    for _ in range(len(hiddenWord)):
+        print(hiddenWord[_], end="")
+        
+def FunctionCheck(check, word):
+    if check==word:
+        win=1
+    else:
+        win=0
+    return win
+
 HANGMAN_STEPS = [
 """
 ------
@@ -82,6 +144,7 @@ usedLetters=[]
 #word=random.choice(words)
 
 #From the internet
+"""
 try:
     response=requests.get('https://random-word-api.herokuapp.com/word')
     words=response.json()
@@ -93,26 +156,37 @@ except requests.exceptions.ConnectionError as err:
         reader=csv.reader(f)
         words=random.choice(list(reader))
         word = random.choice(words)
-x=0
+"""
+word=FunctionHiddenWord()
+"""
 hiddenWord=[]
 while x<len(word):
         hiddenWord.append("_ ")
         x=x+1
+"""
+hiddenWord=FunctionEmptyWord(word)
 #hiddenWord=list(word)
 check=""
 while(True):
     letter=0
+    """
     if check==word:
         win=1
+    """
+    win=FunctionCheck(check, word)
     check=""
+    """
     print(HANGMAN_STEPS[tryes])
     for _ in range(len(hiddenWord)):
         print(hiddenWord[_], end="")
+    """
+    FunctionPrintHangMan(tryes)
     print("\n")
     if tryes==6 or win==1:
         break
     print ("\n")
     print("What character do you chose?")
+    """
     while letter==0:
         userInput=str(input())
         if len(userInput)==1:
@@ -130,16 +204,22 @@ while(True):
             break
         else:
             print("Please enter a single character!")
-        
+    """   
+    usedLetters, tryes=FunctionUserInput(letter, usedLetters, tryes)
+    
+
     for _ in range(len(word)):
         check=check+hiddenWord[_]
     #print("Check is", check)
-    
+
+"""    
 if win==1:
     print("*** Good Job!!!  You win!!! ***")
 else:
     print("The word was **", word, "**")
     print("You lose!!! :(")
+"""
+FunctionWinOrLose(win)
 input('Press ENTER to exit')
     
         
