f80c990b6d7b5f41078bf7c5de0070a260fc9ced
VolmerAndrei <161046263+VolmerAndrei@users.noreply.github.com>
2024-04-13 12:33:45 +0300

    Add files via upload

--- Text_To_Speech.py
+++ Text_To_Speech.py
@@ -1,4 +1,4 @@
-﻿import pypdf
+﻿import PyPDF2 
 import gtts
 from gtts import gTTS
 import pyttsx3
@@ -8,60 +8,109 @@ import googletrans
 import os
 import time
 import pyttsx3
+import re
+
+def Function_Detect_Text_Language():
+    Page_Reader=reader.pages[50]
+    Page_Text=Page_Reader.extract_text()
+
+    langdetect.DetectorFactory.seed = 0
+    Translate_From_Language=langdetect.detect(Page_Text)
+    print(Translate_From_Language)
+    return(Translate_From_Language)
+
+def Function_Translate_Text(page_no, Translate_From_Language):
+    #page_no
+    for i in range(page_no):
+        Page_Reader=reader.pages[i]
+        Page_Text=Page_Reader.extract_text()
+    
+        if "CONTENTS" in Page_Text:
+            pass
+        else:
+            if Translate_From_Language != "ro":
+                Translated_Page=GoogleTranslator(source=Translate_From_Language, target="ro").translate(Page_Text)
+                #Translated_Page=str(Translator.translate(Page_Text, src=Translate_From_Language, dest="ro"))
+                Text=Text+Translated_Page
+            else:
+                Text=Text+Page_Text
+            print("Pagina-",i,"a fost tradusa si scrisa.")
+
+
 
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSTTS_V110_roRO_Andrei")
+engine.setProperty('rate', 145)
 
-
-reader=pypdf.PdfReader(r'C:\Users\Volmer\OneDrive\Desktop\Carti PDF\The-Travels-of-Marco-Polo-Volume-2.pdf')
+reader=PyPDF2.PdfReader(r'C:\Users\Volmer\OneDrive\Desktop\Carti PDF\Procesate\Allans-Wife.pdf')
+search_chapter="CAPITOLUL"
 Text=""
 
 Translator=googletrans.Translator()
 
 page_no=len(reader.pages)
 
-Page_Reader=reader.pages[50]
-Page_Text=Page_Reader.extract_text()
+Translate_From_Language=Function_Detect_Text_Language()
 
-langdetect.DetectorFactory.seed = 0
-Translate_From_Language=langdetect.detect(Page_Text)
-print(Translate_From_Language)
-
-Text=""
-#page_no
-for i in range(39, page_no):
-    Page_Reader=reader.pages[i]
-    Page_Text=Page_Reader.extract_text()
-    
-    if Translate_From_Language != "ro":
-        Translated_Page=GoogleTranslator(source=Translate_From_Language, target="ro").translate(Page_Text)
-        #Translated_Page=str(Translator.translate(Page_Text, src=Translate_From_Language, dest="ro"))
-        Text=Text+Translated_Page
-    else:
-        Text=Text+Page_Text
-    print("Pagina-",i,"a fost tradusa si scrisa.")
+Text=Function_Translate_Text(page_no, Translate_From_Language)
     
 print(len(Text))
 
-n=10000
-Substrings=[]
-for i in range(0, len(Text), n):
-    substring=Text[i:i+n]
-    Substrings.append(substring)
+lista=[]
+lista=re.split('(CAPITOLUL I\n)', Text)
+str_count=lista.count("CAPITOLUL I\n")
+  
+find=0
+for i in lista:
+    if i!="CAPITOLUL I\n" and find!=str_count:
+        lista.remove(i)
+        find=find+1
+ 
+print(lista)
+#lista.pop(0)
+
+Text=""
+for i in lista:
+    Text=Text+i
+    
+lista=re.split('CAPITOLUL', Text)
+print(lista)
+lista.pop(0)
+
+count=1
+for i in lista:
+    Pista="CAPITOLUL"+i
+    print("***PISTA***")
+    print(Pista)
+    folderpath=r'C:\Users\Volmer\OneDrive\Desktop\Carti Audio\Allans-Wife--Carte Audio--Sintetic'
+    filename=f"Pista-{count}--Allans-Wife.mp3"
+    save_path=os.path.join(folderpath, filename)
+    engine.save_to_file(f"Pista {count}"+Pista, save_path)
+    engine.runAndWait()
+    print("Pista", count, "a fost salvata.")
+    count=count+1
+
+
+
+#n=10000
+#Substrings=[]
+#for i in range(0, len(Text), n):
+#    substring=Text[i:i+n]
+#    Substrings.append(substring)
 
 
 
     #len(Substrings)
-for i in range(len(Substrings)):
-    time.sleep(60)
-    folderpath=r'C:\Users\Volmer\OneDrive\Desktop\Carti Audio\The-Travels-of-Marco-Polo-Volume-2--Carte Audio--Sintetic'
-    filename=f"Pista-{i}--The-Travels-of-Marco-Polo-Volume-2.mp3"
-    save_path=os.path.join(folderpath, filename)
+#for i in range(len(Substrings)):
+#    folderpath=r'C:\Users\Volmer\OneDrive\Desktop\Carti Audio\The-Travels-of-Marco-Polo-Volume-2--Carte Audio--Sintetic'
+#    filename=f"Pista-{i}--The-Travels-of-Marco-Polo-Volume-2.mp3"
+#    save_path=os.path.join(folderpath, filename)
     
     #Text_To_Speech=gTTS(text=f"Pista {i}"+Substrings[i], lang='ro', slow=False)
     #Text_To_Speech.save(save_path)
-    engine.save_to_file(f"Pist {i}"+Substrings[i], save_path)
-    engine.runAndWait()
-    print("Pista", i, "a fost salvata.")
-    
+#    engine.save_to_file(f"Pist {i}"+Substrings[i], save_path)
+#    engine.runAndWait()
+#    print("Pista", i, "a fost salvata.")
+
+
